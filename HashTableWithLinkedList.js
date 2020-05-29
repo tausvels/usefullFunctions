8328334aff4d9bcf0c3a04f6c0e8df5765e63f90
tausvels <engr.tausifkhan@gmail.com>
2020-05-28 21:58:27 -0700

    finished the class declaration with test cases

--- HashTableWithLinkedList.js
+++ HashTableWithLinkedList.js
@@ -1,49 +1,82 @@
 class LinkedList {
-  constructor(){this.head = null};
-  // ---- methods -------------------------- //
-  isEmpty(){
-    if (this.head) {return false}
-    return true
-  };
-  lastNode() {
+  head = null;
+  isEmpty() {if (this.head) {return false}; return true;};
+  lastNode(){
     let tempNode = this.head;
-    while(tempNode.getNextNode()) {
-      tempNode = tempNode.getNextNode();
+    while(tempNode.getNext()) {
+      tempNode = tempNode.getNext()
     }
     return tempNode;
-  }
+  };
   insertNode(key, value) {
-    const node = new LinkNode(key, value);
-    if (!this.head) {this.head = node;}
-    else {this.lastNode().insertNextNode(node)}
-  }
-  find(key){
+    const node = new ListNode(key, value);
+    if (!this.head) this.head = node;
+    else this.lastNode().insertNextNode(node)
+  };
+  find(key) {
     let tempNode = this.head;
     while(tempNode) {
-      if (tempNode.getKey() === key) {
-        return tempNode.value
-      }
-      tempNode = tempNode.getNextNode();
+      if (tempNode.getKey() === key) {return tempNode.value()}
+      tempNode = tempNode.getNext();
     }
-    return `No Value with such ${key} found`;
+    return false;
   }
   display(){
     let tempNode = this.head;
     while(tempNode) {
-      console.log(tempNode)
-      tempNode = tempNode.getNextNode();
-    };
+      console.log(tempNode);
+      tempNode = tempNode.getNext();
+    }
   }
 }
 class ListNode {
   constructor(key, value) {
+    this.nextNode = null;
     this.key = key;
     this.value = value;
-    this.nextNode = null
-  };
-  // ---- Defining the class methods -------------//
-  getNextNode(){return this.nextNode}
-  getKey(){return this.key}
-  insertNextNode(node) {this.nextNode = node}
+  }
+  getNext(){return this.nextNode};
+  getKey() {return this.key}
+  insertNextNode(node){this.nextNode = node}
   value(){return this.value}
-}
+};
+
+class HashTable {
+  constructor(size){
+    this.size = size;
+    this.table = [];
+    for (let i = 0; i < size; i++) {
+      this.table[i] = new LinkedList();
+    }
+  };
+  //---- Hashing function ----------------------------------------------- //
+  hash(key){
+    var id = 0;
+    for (let i = 0; i < key.length; i++) {
+      id += key.charCodeAt(i) * 100 - key.charCodeAt(i - 1 < 0 ? 0 : i - 1);
+    }
+    return (id % this.size);
+  };
+  //--------------------------------------------------------------------- //
+  insert(key, value) {
+    let id = this.hash(key);
+    let bucket = this.table[id]
+    bucket.insertNode(key, value)
+  }
+  get(key) {
+    let id = this.hash(key);
+    let item = this.table[id];
+    if (item.head) return item; 
+    else return 'Does not exist'
+  }
+  remove(key) {
+    const id = this.hash(key);
+    this.table[id] = new LinkedList()
+  }
+};
+
+// ---- TEST CASE ------------------------- //
+// const myHashTable = new HashTable(20); // size is 20
+// myHashTable.insert('name', 'Tausif')
+// // myHashTable.remove('name')
+// console.log(myHashTable.get('name'))
